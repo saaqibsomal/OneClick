f72b4d22d769bb3b33aed8c7f46d952643178763
saqibtaj <saaqibsomal@gmail.com>
2024-02-15 19:53:51 +0500

    Add CustomeExtensionMEthod

--- OneClick/Program.cs
+++ OneClick/Program.cs
@@ -1,65 +1,22 @@
-using Microsoft.EntityFrameworkCore;
-using OneClick.Infrastructure.Db;
-using OneClick.Infrastructure.Interface;
-using OneClick.Infrastructure.Repository;
-using OneClick.Models;
-using OneClick.Service;
-using OneClick.Service.Interface;
-using Microsoft.Extensions.Logging;
+using OneClick.Utility.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IUsersRepository, UsersRepository>();
-builder.Services.AddScoped<INewsletterEmailRepository, NewsletterEmailRepository>();
-builder.Services.AddScoped<INewsletterEmailService, NewsletterEmailService>();
+builder.Services.AddCustomServices();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-
-//builder.Services.AddDbContext<MyDbContext>(options =>
-//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddDbContext<OneClickContext>(options =>
-           options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddDBServices(builder.Configuration);
 builder.Services.AddLogging();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOptions();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowOrigin", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-});
-//builder.Services.AddScoped<EmailAddress>
-
-//builder.Services.AddDbContext<MyDbContext>(options=>options)
-
-//builder.Services.Configure<MyAppSettings>(Config.Configuration.GetSection("AppSettings"));
+builder.Services.AddCustomCors();
 var app = builder.Build();
-
-
-
-
-
-// Configure the HTTP request pipeline.
-
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.UseCors("AllowOrigin");
-
 app.MapControllers();
-
 app.Run();
---
+++ OneClick/Utility/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using OneClick.Infrastructure.Interface;
+using OneClick.Infrastructure.Repository;
+using OneClick.Service.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using OneClick.Service;
+
+namespace OneClick.Utility.Extensions
+{
+    public  static class ServiceCollectionExtensions
+    {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUsersRepository, UsersRepository>();
+            services.AddScoped<INewsletterEmailRepository, NewsletterEmailRepository>();
+            services.AddScoped<INewsletterEmailService, NewsletterEmailService>();
+        }
+    }
+}
